f05f625c4f1a3db3292129269d4d174f9dffa2d9
danila.matveev <danila.matveev@itrium.ru>
2023-04-15 16:37:55 +0300

    Created project

---
+++ main.py
@@ -0,0 +1,12 @@
+
+def getAllFromFile():
+    with open("test_image.jpg", "b") as myFile:
+        print(myFile.read())
+
+
+def neuroWorkMethod():
+    pass
+
+
+if __name__ == '__main__':
+    getAllFromFile()
