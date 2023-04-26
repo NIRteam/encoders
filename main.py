ea7d66d9106800c529fb44b827a0eb8e44fd8c68
danila.matveev <danila.matveev@itrium.ru>
2023-04-26 17:50:11 +0300

    #22: внесены изменения из PR

--- main.py
+++ main.py
@@ -1,68 +1,39 @@
 import logging
-import random
 import time
 
-
-def getAllLinesFromFile():
-    with open("data/input/input.txt", "r") as myFile:
-        lines = myFile.readlines()
-        array = [line.strip() for line in lines]
-        return array
+import metrics
+import neuroNetwork
+import utils
 
 
-def neuroWorkMethod(lines):
-    results = []
-    for line in lines:
+def main():
+    logging.basicConfig(level=logging.DEBUG, filename="logfile.log", filemode="w")
+    resultOfCheckDir = utils.checkDir()
+
+    if not resultOfCheckDir:
+        logging.debug("Directories created")
+        return 0
+
+    resMetrics = []
+    resTimes = []
+    logging.debug("Directories already exist")
+    commands = utils.getAllCommands()
+
+    for command in commands:
         startTime = time.time()
-        numberOfSameSymbols = 0
+        transformedCommand = utils.transformText(command)
 
-        newLine = neuroEmulation(line)
-        minLengthOfComparedStrings = min(len(line), len(newLine))
+        neuroNetwork.runEncoder()
+        neuroNetwork.runDecoder()
 
-        for i in range(minLengthOfComparedStrings):
-            if ord(line[i]) == ord(newLine[i]):
-                numberOfSameSymbols += 1
+        newLine = utils.neuroEmulation(command)
 
-        accuracy = numberOfSameSymbols / max(len(line), len(newLine))
+        resMetrics.append(metrics.neuroWorkMethod(command, newLine))
+        resTimes.append(time.time() - startTime)
 
-        results.append([accuracy, len(line), len(newLine), (time.time() - startTime), line, newLine])
-    return results
-
-
-def neuroEmulation(line):
-    result = ""
-    randomObj = random.Random()
-    for symbol in line:
-        if randomObj.randint(a=1, b=50) == 42:
-            symbol = '3'
-        elif randomObj.randint(a=1, b=50) == 25:
-            symbol = ''
-        result += symbol
-    return result
+    utils.writeMetricsInFile(resMetrics, resTimes)
 
 
 if __name__ == '__main__':
-    with open("metrics.txt", "w") as metricsFile:
-        logging.basicConfig(level=logging.DEBUG, filename="logfile.log", filemode="w")
-        startTime = time.time()
 
-        lines = getAllLinesFromFile()
-        results = neuroWorkMethod(lines)
-        logging.debug("\n---------------------------------------------------------------------\n"
-                      + "                               results                               \n"
-                      + "---------------------------------------------------------------------")
-
-        numberOfResult = 1
-        for result in results:
-            metricsMsg = ("\nresult № " + str(numberOfResult) + ":" +
-                          "\n    accuracy: %.3f" % result[0] +
-                          "\n    start line length: " + str(result[1]) +
-                          "\n    result line length: " + str(result[2]) +
-                          "\n    time spent on processing: %.6f" % result[3] +
-                          "\n    start line: " + result[4] +
-                          "\n    new line: " + result[5] + "\n")
-            logging.debug(metricsMsg)
-            metricsFile.write(metricsMsg)
-            numberOfResult += 1
-
-        logging.debug("time for all processing: %.6f" % (time.time() - startTime))
+    main()
