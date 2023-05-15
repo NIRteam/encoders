f06f4fea91fa057d8caf85d72ef4b4773294277b
danila.matveev <danila.matveev@itrium.ru>
2023-05-15 20:01:26 +0300

    #40: спаренны функции для кодировки данных

--- code_utils.py
+++ code_utils.py
@@ -1,9 +1,15 @@
 import bchlib
+import reedsolo
 
 
 def create_coder(config):
+    wrong_mode_raise(config)
+    
     if config.get('mode', 'mode') == "bch":
         return bchlib.BCH(config.getint('bch', 'poly'), config.getint('bch', 't'))
+
+    if config.get('mode', 'mode') == "rs":
+        return reedsolo.RSCodec(config.getint("rs", "ecc"), config.getint("rs", "max_size"))
 
 
 def encode(command, coder, config):
@@ -13,9 +19,20 @@ def encode(command, coder, config):
         pkg = data + ecc
         return pkg
 
+    if config.get('mode', 'mode') == "rs":
+        return coder.encode(command.encode("utf-8"))
+
 
 def decode(pkg, coder, config):
     if config.get('mode', 'mode') == "bch":
         data, ecc = pkg[:-coder.ecc_bytes], pkg[-coder.ecc_bytes:]
         val = coder.decode(data, ecc)
         return val[1].decode("utf-8")
+
+    if config.get('mode', 'mode') == "rs":
+        return coder.decode(pkg)[0].decode("utf-8")
+
+
+def wrong_mode_raise(config):
+    if config.get('mode', 'mode') != "rs" and config.get('mode', 'mode') != "rs":
+        raise Exception("Wrong parameter for coder")
