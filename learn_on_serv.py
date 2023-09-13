f6379cf927c13901084ec0b3936cf71a7ac237c8
danila.matveev <danila.matveev@itrium.ru>
2023-09-13 18:34:43 +0300

    #73: добавлено логирование для обучения на сервере

--- learn_on_serv.py
+++ learn_on_serv.py
@@ -1,3 +1,5 @@
+import logging
+
 import model_factory
 from keras.callbacks import CSVLogger, EarlyStopping
 
@@ -12,35 +14,43 @@ class ParamsForBCH:
         self.n = n
 
 
+logging.basicConfig(level=logging.DEBUG, filename="logfile.log", filemode="w")
+
 try:
     for param in LIST_OF_PARAMS:
-        iterator = MyIterator(param.k, param.n)
-        num_samples = 2 ** param.n
-
-        input_shape = (param.n,)
-        hidden_size = param.n + 1
-
-        list_of_models = []
-        for name_of_model in LIST_OF_NAMES_OF_MODELS:
-            uncompiled_model = model_factory.new_model(
-                name_of_model, input_shape, INPUT_LAYERS, OUTPUT_LAYERS, hidden_size
-            )
-            list_of_models.append(uncompiled_model.compile())
-
         try:
-            for model in list_of_models:
-                csv_logger = CSVLogger(PATH_TO_LOGGER, separator=',', append=True)
-                early_stopping = EarlyStopping(monitor='errors', mode='auto', min_delta=0, patience=0,
-                                               restore_best_weights=True, baseline=0)
+            iterator = MyIterator(param.k, param.n)
+            num_samples = 2 ** param.n
 
-                history = model.fit(iterator, steps_per_epoch=num_samples, epochs=1,
-                                    callbacks=[csv_logger, early_stopping])
+            input_shape = (param.n,)
+            hidden_size = param.n + 1
 
-                model.save_weights(PATH_TO_WEIGHTS)
+            list_of_models = []
+            for name_of_model in LIST_OF_NAMES_OF_MODELS:
+                uncompiled_model = model_factory.new_model(
+                    name_of_model, input_shape, INPUT_LAYERS, OUTPUT_LAYERS, hidden_size
+                )
+                list_of_models.append(uncompiled_model.compile())
+
+            try:
+                for model in list_of_models:
+                    try:
+                        csv_logger = CSVLogger(PATH_TO_LOGGER, separator=',', append=True)
+                        early_stopping = EarlyStopping(monitor='errors', mode='auto', min_delta=0, patience=0,
+                                                       restore_best_weights=True, baseline=0)
+
+                        history = model.fit(iterator, steps_per_epoch=num_samples, epochs=1,
+                                            callbacks=[csv_logger, early_stopping])
+
+                        model.save_weights(PATH_TO_WEIGHTS)
+                    except Exception as err:
+                        logging.error(f"Error while processing model = {model}. Reason: {err}")
+
+            except Exception as err:
+                logging.error(f"Error while processing models. Reason: {err}")
+
         except Exception as err:
-            # TODO: добавить логирование
-            pass
+            logging.error(f"Error while processing combination n = {param.n}, k = {param.k}. Reason: {err}")
 
 except Exception as err:
-    # TODO: добавить логирование
-    pass
+    logging.error(f"Error while whole process. Reason: {err}")
