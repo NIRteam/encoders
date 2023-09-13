dddf5e38ef4f0abc0f7fffc64595123622f0e1ce
danila.matveev <danila.matveev@itrium.ru>
2023-09-13 17:49:31 +0300

    #73: добавлен список с БЧХ

--- constants/constant.py
+++ constants/constant.py
@@ -14,8 +14,24 @@ INPUT_LAYERS = 5
 
 OUTPUT_LAYERS = 2
 
-# TODO заполнить табличку для БЧХ
-LIST_OF_PARAMS = [ParamsForBCH(4, 7)]
+LIST_OF_PARAMS = [ParamsForBCH(4, 7), ParamsForBCH(11, 15), ParamsForBCH(7, 15), ParamsForBCH(5, 15),
+                  ParamsForBCH(26, 31), ParamsForBCH(21, 31), ParamsForBCH(16, 31), ParamsForBCH(11, 31),
+                  ParamsForBCH(6, 31), ParamsForBCH(57, 63), ParamsForBCH(51, 63), ParamsForBCH(45, 63),
+                  ParamsForBCH(39, 63), ParamsForBCH(36, 63), ParamsForBCH(30, 63), ParamsForBCH(24, 63),
+                  ParamsForBCH(18, 63), ParamsForBCH(16, 63), ParamsForBCH(10, 63), ParamsForBCH(7, 63),
+                  ParamsForBCH(120, 127), ParamsForBCH(113, 127), ParamsForBCH(106, 127), ParamsForBCH(99, 127),
+                  ParamsForBCH(92, 127), ParamsForBCH(85, 127), ParamsForBCH(78, 127), ParamsForBCH(71, 127),
+                  ParamsForBCH(64, 127), ParamsForBCH(57, 127), ParamsForBCH(50, 127), ParamsForBCH(43, 127),
+                  ParamsForBCH(36, 127), ParamsForBCH(29, 127), ParamsForBCH(22, 127), ParamsForBCH(15, 127),
+                  ParamsForBCH(8, 127), ParamsForBCH(247, 255), ParamsForBCH(239, 255), ParamsForBCH(231, 255),
+                  ParamsForBCH(223, 255), ParamsForBCH(215, 255), ParamsForBCH(207, 255), ParamsForBCH(199, 255),
+                  ParamsForBCH(191, 255), ParamsForBCH(187, 255), ParamsForBCH(179, 255), ParamsForBCH(171, 255),
+                  ParamsForBCH(163, 255), ParamsForBCH(155, 255), ParamsForBCH(147, 255), ParamsForBCH(139, 255),
+                  ParamsForBCH(131, 255), ParamsForBCH(123, 255), ParamsForBCH(115, 255), ParamsForBCH(107, 255),
+                  ParamsForBCH(99, 255), ParamsForBCH(91, 255), ParamsForBCH(87, 255), ParamsForBCH(79, 255),
+                  ParamsForBCH(71, 255), ParamsForBCH(63, 255), ParamsForBCH(55, 255), ParamsForBCH(47, 255),
+                  ParamsForBCH(45, 255), ParamsForBCH(37, 255), ParamsForBCH(29, 255), ParamsForBCH(21, 255),
+                  ParamsForBCH(13, 255), ParamsForBCH(9, 255)]
 
 LIST_OF_NAMES_OF_MODELS = ['ae', 'vae', 'cae', 'mae']
 
